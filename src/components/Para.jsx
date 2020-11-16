e0e1cc7d73c7567f9d900b02563f6072b9d1de69
pratik kumar <kumarpratik861@gmail.com>
2020-11-17 01:48:16 +0530

    CAHNGES

---
+++ src/components/Para.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Para() {
+  return (
+    <>
+      <p id="para">
+        Hello, I've learnt to use the full-stack evaluation tool. This makes me
+        so happy
+      </p>
+    </>
+  );
+}
